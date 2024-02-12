3c3992ebb0b8a09ff2878cbe4690f9949fbd18be
João Domingos Pinheiro Neto <56659658+joaonetogit@users.noreply.github.com>
2024-02-12 12:51:47 -0300

    feat: animate hero

--- src/components/SectionHero/AreaTextHero.js
+++ src/components/SectionHero/AreaTextHero.js
@@ -12,42 +12,56 @@ import AppleStore from "public/apple.svg";
 import useAreaTextHero from "./useAreaTextHero";
 
 const AreaTextHero = () => {
-  useAreaTextHero();
+  const { imageCircleRef, areaTextRef, googleRef, appleRef } = useAreaTextHero();
 
   return (
     <>
-      <h3 className="flex items-center gap-2 text-sm mb-3 @tablet:text-base @desktop:text-xl">
-        <Image src={iconBoost} alt="Icon Boost" />{" "}
-        <span>Adentre um reino de possibilidades infinitas</span>
-      </h3>
-      <h1 className="text-5xl/normal text-center font-poppins mb-4 @desktop:text-7xl">
-        Explore um Universo
-        <div className="inline-block align-middle mx-2 p-1 bg-white bg-opacity-5 rounded-full border border-white border-opacity-5 w-24 @desktop:w-32">
+      <div className="flex flex-col items-center" ref={areaTextRef}>
+        <h3 className="flex items-center gap-2 text-sm mb-3 @tablet:text-base @desktop:text-xl">
+          <Image src={iconBoost} alt="Icon Boost" />{" "}
+          <span>Adentre um reino de possibilidades infinitas</span>
+        </h3>
+        <h1 className="text-5xl/normal text-center font-poppins mb-4 @desktop:text-7xl">
+          Explore um Universo
+          <div className="inline-block align-middle mx-2 p-1 bg-white bg-opacity-5 rounded-full border border-white border-opacity-5 w-24 @desktop:w-32">
+            <Image
+              src={nftSmall}
+              alt="NFT Small"
+              className="w-11 @desktop:w-auto"
+              ref={imageCircleRef}
+            />
+          </div>
+          Digital de
           <Image
-            src={nftSmall}
-            alt="NFT Small"
-            className="w-11 @desktop:w-auto"
+            src={nftDoubleSmall}
+            alt="NFT Double Small"
+            className="inline-block w-16 mx-2 @desktop:w-auto"
           />
-        </div>
-        Digital de
-        <Image
-          src={nftDoubleSmall}
-          alt="NFT Double Small"
-          className="inline-block w-16 mx-2 @desktop:w-auto"
-        />
-        Colecionáveis Únicos!
-      </h1>
-      <p className="w-full text-center text-sm text-white text-opacity-80 mb-6 @tablet:text-base @tablet:max-w-3xl @desktop:text-xl @desktop:max-w-2xl @desktop:mb-9">
-        Adentre um reino de possibilidades infinitas e mergulhe nas coleções de
-        NFT, onde a arte, a cultura e a inovação se unem para criar experiências
-        verdadeiramente exclusivas.
-      </p>
+          Colecionáveis Únicos!
+        </h1>
+        <p className="w-full text-center text-sm text-white text-opacity-80 mb-6 @tablet:text-base @tablet:max-w-3xl @desktop:text-xl @desktop:max-w-2xl @desktop:mb-9">
+          Adentre um reino de possibilidades infinitas e mergulhe nas coleções
+          de NFT, onde a arte, a cultura e a inovação se unem para criar
+          experiências verdadeiramente exclusivas.
+        </p>
+      </div>
+
       <div className="flex flex-col items-center gap-4 @tablet:flex-row @desktop:gap-6">
         <button>
-          <Image src={GooglePlay} alt="GooglePlay" />
+          <Image
+            src={GooglePlay}
+            alt="GooglePlay"
+            ref={googleRef}
+            className="opacity-0"
+          />
         </button>
         <button>
-          <Image src={AppleStore} alt="AppleStore" />
+          <Image
+            src={AppleStore}
+            alt="AppleStore"
+            ref={appleRef}
+            className="opacity-0"
+          />
         </button>
       </div>
     </>
--- src/components/SectionHero/useAreaTextHero.js
+++ src/components/SectionHero/useAreaTextHero.js
@@ -1,14 +1,81 @@
 import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 const useAreaTextHero = () => {
-  const subtitleRef = useRef(null);
-  const titleRef = useRef(null);
-  const textRef = useRef(null);
-  const storesRef = useRef(null);
-  const imagesLeftRef = useRef(null);
-  const imagesRightRef = useRef(null);
+  const areaTextRef = useRef(null);
+  const googleRef = useRef(null);
+  const appleRef = useRef(null);
+  const imageCircleRef = useRef(null);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const areaTextRefCurrent = areaTextRef.current;
+    const googleRefCurrent = googleRef.current;
+    const appleRefCurrent = appleRef.current;
+    const imageCircleRefCurrent = imageCircleRef.current;
+
+    const tl = gsap.timeline();
+
+    const patternAnimationTo = {
+      opacity: 1,
+      duration: 1,
+      ease: "power3.out",
+    };
+
+    tl.fromTo(
+      areaTextRefCurrent,
+      {
+        opacity: 0,
+        y: 100,
+      },
+      {
+        y: 0,
+        ...patternAnimationTo,
+      },
+      "init",
+    )
+      .fromTo(
+        googleRefCurrent,
+        {
+          opacity: 0,
+          x: -100,
+        },
+        {
+          x: 0,
+          ...patternAnimationTo,
+        },
+      )
+      .fromTo(
+        appleRefCurrent,
+        {
+          opacity: 0,
+          x: 100,
+        },
+        {
+          x: 0,
+          ...patternAnimationTo,
+        },
+        "-=1",
+      )
+      .fromTo(
+        imageCircleRefCurrent,
+        {
+          opacity: 0,
+          xPercent: 90,
+        },
+        {
+          xPercent: 0,
+          ...patternAnimationTo,
+        },
+        "init+=0.7",
+      );
+  }, []);
+
+  return {
+    areaTextRef,
+    googleRef,
+    appleRef,
+    imageCircleRef
+  };
 };
 
 export default useAreaTextHero;
